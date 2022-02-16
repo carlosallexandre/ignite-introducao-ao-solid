1d18d25726d267fc9def448d0f4c2ff54ae2ed84
carlosallexandre <carlosallexandre@pop-os.localdomain>
2022-02-16 18:07:57 -0300

    docs: add api-docs with swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import path from "path";
+import swagger from "swagger-ui-express";
+import YAML from "yamljs";
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
+const swaggerDocument = YAML.load(path.resolve(__dirname, "swagger.yaml"));
+
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocument));
 
 export { app };
